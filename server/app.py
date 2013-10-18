6df1b74cace0158437854cac642780512357624f
Shaohan <shaohan@ischool.berkeley.edu>
2013-10-18 16:26:58 -0700

    [Fixed] alias to display in home.html

--- server/app.py
+++ server/app.py
@@ -16,17 +16,16 @@ db = shelve.open("shorten.db")
 # Home Resource:
 # Only supports the GET method, returns a homepage represented as HTML
 ###
-@app.route('/home', methods=['GET'])
-def home():
-    """Builds a template based on a GET request, with some default
-    arguements"""
-    index_title = request.args.get("title", "i253")
-    hello_name = request.args.get("name", "Jim")
-    return flask.render_template(
-            'home.html',
-            title=index_title,
-            name=hello_name)
-
+#@app.route('/home', methods=['GET'])
+#def home():
+#    """Builds a template based on a GET request, with some default
+#    arguements"""
+#    index_title = request.args.get("title", "i253")
+#    hello_name = request.args.get("name", "Jim")
+#    return flask.render_template(
+#            'home.html',
+#            title=index_title,
+#            name=hello_name)
 
 ###
 # Wiki Resource:
@@ -59,9 +58,13 @@ def shorts_post():
     alias = alias1.encode('ascii','ignore')
     db[alias] = url
     return flask.render_template(
-            'shorts.html',
-            alias=alias,
-            url=url)
+        'home.html',
+        alias=alias)
+
+#    return flask.render_template(
+#            'shorts.html',
+#            alias=alias,
+#            url=url)
 
 @app.route('/short/<alias>', methods=['GET'])
 def short_get(alias):
@@ -83,27 +86,28 @@ def short_get(alias):
 # TODO: The representation for this resource is broken. Fix it!
 # Set the correct MIME type to be able to view the image in your browser
 ##/
-@app.route('/i253')
-def i253():
-    """Returns a PNG image of madlibs text"""
-    relationship = request.args.get("relationship", "friend")
-    name = request.args.get("name", "Jim")
-    adjective = request.args.get("adjective", "fun")
+#@app.route('/i253')
+#def i253():
+#    """Returns a PNG image of madlibs text"""
+#    relationship = request.args.get("relationship", "friend")
+#    name = request.args.get("name", "Jim")
+#    adjective = request.args.get("adjective", "fun")
 
-    resp = flask.make_response(
-            check_output(['convert', '-size', '600x400', 'xc:transparent',
-                '-frame', '10x30',
-                '-font', '/usr/share/fonts/liberation/LiberationSerif-BoldItalic.ttf',
-                '-fill', 'black',
-                '-pointsize', '32',
-                '-draw',
-                  "text 30,60 'My %s %s said i253 was %s'" % (relationship, name, adjective),
-                '-raise', '30',
-                'png:-']), 200);
+#    resp = flask.make_response(
+ 
+#           check_output(['convert', '-size', '600x400', 'xc:transparent',
+#                '-frame', '10x30',
+#                '-font', '/usr/share/fonts/liberation/LiberationSerif-BoldItalic.ttf',
+#                '-fill', 'black',
+#                '-pointsize', '32',
+#                '-draw',
+#                  "text 30,60 'My %s %s said i253 was %s'" % (relationship, name, adjective),
+#                '-raise', '30',
+#                'png:-']), 200);
     # Comment in to set header below
     # resp.headers['Content-Type'] = '...'
 
-    return resp
+#    return resp
 
 
 if __name__ == "__main__":
